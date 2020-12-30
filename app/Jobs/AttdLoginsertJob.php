cd0deef69489a3b3544de42111afae71444fbc66
anand1606 <anand.j.acharya@gmail.com>
2020-12-30 14:37:43 +0530

    2020-12-30: Bug Fixed in job : Multiline AttdLog , array explode failed.

--- app/Jobs/AttdLoginsertJob.php
+++ app/Jobs/AttdLoginsertJob.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Transaction;
 use App\Models\Terminal;
-
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
 class AttdLoginsertJob implements ShouldQueue
@@ -38,38 +38,55 @@ class AttdLoginsertJob implements ShouldQueue
      */
     public function handle()
     {
-      $lines = explode(PHP_EOL, $this->data);
+      $lines = explode("\n", $this->data);
+       //Log::debug($lines);
       $ip =  $this->ip;
-      
+
       $machine = Terminal::where('ip_address', $ip)->where('approved',true)->first();
       if($machine)
       {
+        //$i = 0; $inserted = 0;
 
         foreach ($lines as $line)
         {
-            $ary = explode("\t", $line);
-            $empcode = $ary[0];
-            $time = $ary[1];
-            $verifymode = $ary[3];
+           //$i = $i + 1;
 
-            if(Transaction::where('ip_address',$ip)->
-              where('empunqid', $empcode)->
-              where('punchdate',$time)->
-              where('ioflg',$machine->ioflg)->
-              count() <= 0)
+            $ary = explode("\t", $line);
+            if(count($ary) >= 4)
             {
-              $transaction = Transaction::Create(
-                [
-                  'ip_address' => $ip,'empunqid' => $empcode,
-                  'punchdate' => $time, 'verifymode' => $verifymode ,
-                  'ioflg' => $machine->ioflg , 'exported' => 0
-                ]
-              );
-              //Log::debug('record inserted');
-            }
+              $empcode = $ary[0];
+              $time = $ary[1];
+              $verifymode = $ary[3];
+              //print_r($ary);
+              if(Transaction::where('ip_address',$ip)->
+                where('empunqid', $empcode)->
+                where('punchdate',$time)->
+                where('ioflg',$machine->ioflg)->
+                count() <= 0)
+              {
+                $transaction = Transaction::Create(
+                  [
+                    'ip_address' => $ip,'empunqid' => $empcode,
+                    'punchdate' => $time, 'verifymode' => $verifymode ,
+                    'ioflg' => $machine->ioflg , 'exported' => 0
+                  ]
+                );
+                  // $inserted += 1;
+                  //Log::debug($transaction);
+              }//if record search
+
+              //print($inserted);
+            }//endif array
 
           }//end for loop
 
+          unset($lines);
+          unset($ary);
+          unset($empcode);
+          unset($time);
+          unset($verifymode);
+          unset($ip);
+          unset($machine);
           return 1;
       }// endif terminal found..
 
